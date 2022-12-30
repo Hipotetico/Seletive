557781a9e2246414ab632040b4fb3988ade8a1de
Leandro Prado <106562344+Hipotetico@users.noreply.github.com>
2022-12-29 22:18:44 -0300

    Criação Tabelas BD

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Tecnologias, Empresa
+
 # Register your models here.
+
+admin.site.register(Tecnologias)
+admin.site.register(Empresa)
--- empresa/models.py
+++ empresa/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tecnologia
+
 class Empresa(models.Model):
 
     choices_nicho_mercado = (
@@ -9,9 +15,15 @@ class Empresa(models.Model):
         ('N', 'Nutrição'),
         ('D', 'Design')
     )
+
+    logo = models.ImageField(upload_to='logo_empresa')
     nome = models.CharField(max_length=30)
     email = models.EmailField()
     cidade = models.CharField(max_length=30)
     endereco = models.CharField(max_length=30)
     caracteristica_empresa = models.TextField()
     nicho_mercado = models.CharField(max_length=3, choices=choices_nicho_mercado)
+    tecnologia = models.ManyToManyField(Tecnologias)
+
+    def __str__(self):
+        return self.nome
