2878efa8bae8b371cea590ae28a0a184f26ddc4c
Leandro Prado <106562344+Hipotetico@users.noreply.github.com>
2023-01-04 19:16:10 -0300

    Adicionado opçoes dentro do formulario

--- empresa/views.py
+++ empresa/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Tecnologias, Empresa
 
 # Create your views here.
 
 def nova_empresa(response):
-    return render(response, 'nova_empresa.html')
+    techs = Tecnologias.objects.all()
+    nichos = Empresa.choices_nicho_mercado
+    
+    return render(response, 'nova_empresa.html', {'techs': techs, 'nichos': nichos})
