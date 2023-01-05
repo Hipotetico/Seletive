e87bef9b7ddc4697087610109d94c9c55199ca9a
Leandro Prado <106562344+Hipotetico@users.noreply.github.com>
2023-01-04 21:32:26 -0300

    Coleta dados method POST

--- empresa/views.py
+++ empresa/views.py
@@ -4,8 +4,19 @@ from .models import Tecnologias, Empresa
 
 # Create your views here.
 
-def nova_empresa(response):
-    techs = Tecnologias.objects.all()
-    nichos = Empresa.choices_nicho_mercado
-    
-    return render(response, 'nova_empresa.html', {'techs': techs, 'nichos': nichos})
+def nova_empresa(request):
+    if request.method == 'GET':
+        techs = Tecnologias.objects.all()
+        nichos = Empresa.choices_nicho_mercado
+        
+        return render(request, 'nova_empresa.html', {'techs': techs, 'nichos': nichos})
+
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        cidade = request.POST.get('cidade')
+        endereco = request.POST.get('endereco')
+        nicho = request.POST.get('nicho')
+        caracteristicas = request.POST.get('caracteristicas')
+        
+        return HttpResponse('Teste')
